870512519d9d12292660164f72f2c31cdfadeab1
Ashwin <mahesh.ashwin1998@gmail.com>
2018-07-09 11:33:10 -0700

    localized task adding complete

--- BeastMode/ViewController.swift
+++ BeastMode/ViewController.swift
@@ -9,14 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var tasks=["Go to the gym", "Create a wireframe", "Make an ERD"]
 
-    @IBOutlet weak var TaskTextField: UIStackView!
+    
+    @IBOutlet weak var taskTextField: UITextField!
     @IBOutlet weak var tableView: UITableView!
     @IBAction func beastButtonPushed(_ sender: UIButton) {
-        
+        if let newTask=taskTextField.text{
+            var allSpaces=true
+            for char in newTask{
+                if char != " "{
+                    allSpaces=false
+                    break;
+                }
+            }
+            if !allSpaces{
+                tasks.append(newTask)
+            }
+        }
+        tableView.reloadData()
+        taskTextField.text=""
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource=self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -26,5 +42,15 @@ class ViewController: UIViewController {
     }
 
 
+}
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section:Int)->Int{
+        return tasks.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath:IndexPath)->UITableViewCell{
+        let cell=tableView.dequeueReusableCell(withIdentifier:"MyCell", for: indexPath)
+        cell.textLabel?.text=tasks[indexPath.row]
+        return cell
+    }
 }
 
